a16d0a9ed7eebf6bf63a2cdb55303d8089fc0902
devandref <yoandredepaula@gmail.com>
2023-12-27 11:10:05 -0300

    commit inicial

---
+++ src/main/java/io/github/devandref/kafkaalura/KafkaAluraApplication.java
@@ -0,0 +1,13 @@
+package io.github.devandref.kafkaalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAluraApplication.class, args);
+	}
+
+}

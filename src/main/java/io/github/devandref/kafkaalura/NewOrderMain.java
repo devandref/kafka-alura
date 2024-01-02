4dfeccfe4c2e821169a474660a42d154aba5ec99
André Ferreira de Paula <148173697+devandref@users.noreply.github.com>
2024-01-02 11:17:01 -0300

    Feature/aula 03 (#6)
    * Develop (#5)
    
    * Feature/aula-01 (#2)
    
    * include: criação do producer + configuração do projeto
    
    * include: criação do consumer de fraude
    
    * Feature/aula 02 (#3)
    
    * include: criação do producer + configuração do projeto
    
    * include: criação do consumer de fraude
    
    * include: keys e partições para balanceamento de carga de processamento de eventos
    
    * Include: divisão em camadas

--- src/main/java/io/github/devandref/kafkaalura/NewOrderMain.java
+++ src/main/java/io/github/devandref/kafkaalura/NewOrderMain.java
@@ -1,40 +1,20 @@
 package io.github.devandref.kafkaalura;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.time.LocalDate;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var key = UUID.randomUUID().toString();
-        var value = key + "id_pedido, id_usuario, valor_compra";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + LocalDate.now();
+                var email = key + "@gmail.com";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
-            System.out.println("Sucesso enviado: " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
-        var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+        }
     }
 }

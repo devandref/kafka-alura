bd16bcfcbeaeb3e212c274acc994044fd809e345
André Ferreira de Paula <148173697+devandref@users.noreply.github.com>
2024-01-06 18:52:52 -0300

    include: custom serializer (#9)

--- src/main/java/io/github/devandref/kafkaalura/NewOrderMain.java
+++ src/main/java/io/github/devandref/kafkaalura/NewOrderMain.java
@@ -1,19 +1,26 @@
 package io.github.devandref.kafkaalura;
 
-import java.time.LocalDate;
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + LocalDate.now();
-                var email = key + "@gmail.com";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = Math.random() * 5000 + 1;
+
+                    var order = new Order(userId, orderId, new BigDecimal(amount));
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank you for your order! We're processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
